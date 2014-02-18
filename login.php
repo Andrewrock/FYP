4bd4e71046e0ce6eaa34f59f1deee114aaac97bf
Andrewrock <arock91@icloud.com>
2014-02-18 00:38:41 +0000

    Updates to links

--- login.php
+++ login.php
@@ -13,11 +13,15 @@ $_SESSION['form_token'] = $form_token;
 <html>
 <head>
   <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-  
-  <link rel="stylesheet" href="css/login.css">
-  <link rel="stylesheet" href="css/font-awesome.min.css">
- 
-  
+
+  		<link rel="stylesheet" href="css/login.css">
+  		<link rel="stylesheet" href="css/font-awesome.min.css">
+  		<link href="css/parsley.css" rel="stylesheet" type="text/css">
+		<script src="http://code.jquery.com/jquery-latest.js"></script>
+		<script src="scripts/bootstrap.min.js"></script>
+		<script type="text/javascript" src="scripts/jquery.min.js"></script>
+		<script type="text/javascript" src="scripts/parsley.js"></script>
+
 </head>
 <body>
   <h2>Welcome to Uvote</h2>
@@ -25,16 +29,15 @@ $_SESSION['form_token'] = $form_token;
   <div class="login">
   <h1>Let's get started.</h1>
   <p></p>
-  <form action="login_submit.php" method="post">
+  <form action="login_submit.php" method="post" data-validate="parsley">
     <div class="input">
       <div class="blockinput">
-      
-        <i class="fa fa-keyboard-o"></i><input type="mail" id="student_username" name="student_username" placeholder="Student Number">
-       
+
+       <i class="fa fa-keyboard-o"></i><input type="mail" required data-trigger="change" id="student_username" name="student_username" placeholder="Student Number">
       </div>
       <div class="blockinput">
-        <i class="fa fa-pencil"></i><input type="password" id="student_password" name="student_password" placeholder="Password">
-       
+        <i class="fa fa-pencil"></i><input type="password" required data-trigger="change" id="student_password" name="student_password" placeholder="Password">
+
       </div>
     </div>
     <input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
@@ -46,4 +49,3 @@ $_SESSION['form_token'] = $form_token;
    <p class="signup"><a href="Adminlogin.php">Admin member</a></p>
     </body>
 </html>
-  
